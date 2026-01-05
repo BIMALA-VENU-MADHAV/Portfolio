bc52224e06d96317b6174caa625ce929ddd70f90
BIMALA VENU MADHAV <venubimala1234@gmail.com>
2026-01-05 11:43:19 +0530

    Portfolio

---
+++ Portfolio/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className="home-container">
+     <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p>This is the home page of my portfolio website.</p>
+    </div>
+  );
+}
